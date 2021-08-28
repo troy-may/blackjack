31f4dccbf3fbf8ac2f0312784cad9d4f7cc2116e
troy-may <troy.may@fortuneunmasked.com>
2021-08-28 23:10:31 +1200

    added sum message

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 let firstCard = 10
-let secondCard = 10
+let secondCard = 9
 let sum = firstCard + secondCard
 let hasBlackJack = false
 let isAlive = true
 let message = ""
+let messageEl = document.getElementById("message-el")
+let sumEl = document.getElementById("sum-el")
 
 function startGame() {
+    sumEl.textContent = "Sum: " + sum
     if (sum <= 20) {
         message = "Do you want to draw a new card?"
     } else if (sum === 21) {
@@ -15,5 +18,5 @@ function startGame() {
         message = "Damn you're out of the game"
         isAlive = false
 }
-console.log(message)
+messageEl.textContent = message
 }
