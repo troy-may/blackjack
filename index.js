95a18d736bb9fb12193a5c44e8127aade99d8321
troy-may <troy.may@fortuneunmasked.com>
2021-08-30 22:47:08 +1200

    made a card array

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 let firstCard = 10
 let secondCard = 6
+let cards = [firstCard, secondCard]
 let sum = firstCard + secondCard
 let hasBlackJack = false
 let isAlive = true
@@ -13,7 +14,7 @@ function startGame() {
 }
 
 function renderGame() {
-    cardsEl.textContent = "Cards: " + firstCard + " " + secondCard
+    cardsEl.textContent = "Cards: " + cards[0] + " " + cards[1]
     sumEl.textContent = "Sum: " + sum
     if (sum <= 20) {
         message = "Do you want to draw a new card?"
@@ -30,5 +31,7 @@ messageEl.textContent = message
 function newCard() {
     let card = 1
     sum += card
+    cards.push(card)
+    console.log(cards)
     renderGame()
 }
